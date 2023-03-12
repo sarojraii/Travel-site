8a4db866400a0299891937f844f38d53fe3954dd
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-12 08:13:10 +0545

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Link } from "react-scroll";
 
 export default function Hero(){
     
 return(
-    <section className="hero-container">
+    <section className="hero-container" id="Hero">
        <div className="content">
             <h1>ADVENTURE <span>AWAITS</span> </h1>
             <h3><span>WELCOME</span> TO MY SITE</h3>
@@ -12,8 +12,10 @@ return(
         
         </div>
         <div className="hero-btn">
-        <button>
+        <button className="btn-scroll">
+            <Link to="sign" spy={true} smooth={true} offset={50} duration={800}>
                 GET STARTED
+            </Link>
             </button>
            
             </div>
