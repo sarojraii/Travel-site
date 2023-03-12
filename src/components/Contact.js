d4795334f30cb070d82a8039fa25421ece9052f5
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-12 10:08:01 +0545

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,29 @@ import React from "react";
 
 function Contact(){
     return(
-        <div id="Contact">
-            
+        <div className="contact" id="Contact">
+            <div className="c-page">
+                <label className="c-us">CONTACT US</label>
+                <div className="c-main">
+                    <div className="c-info">
+                        <h3 className="c-head">Get in touch</h3>
+                        <p className="c-para">Email: raii.saroj21@gmail.com</p>
+                        <p className="c-paraa">Phone: +977 XXXXXXXX</p>
+                        <label className="c-paraaa">If you have any further enquires,<br></br>
+                           please fill the given form.</label>
+ 
+                    </div>
+                    <div className="c-form">
+                        <p className="c-type"> Full Name:</p>
+                        <input type="Text" placeholder='Enter your Name' />
+                        <p className="c-type"> Email:</p>
+                        <input type="Text" placeholder='Enter your mail' />
+                        <p className="c-type"> Comment:</p>
+                        <input className="c-comment" type="Text" placeholder='Write down your Comments' />
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
