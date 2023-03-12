a6dce79c41d453b3656babae51e7779937431cbe
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-12 08:12:35 +0545

    Update Service.js

--- src/components/Service.js
+++ src/components/Service.js
@@ -5,16 +5,17 @@ const Service = (props) => {
     return(
         
         
-        <div className="service">
+        <div className="service" id="Service">
             <div className="service-body">
                 
-          <div>
+          <div id="smooth">
             <h3 className="data_title">{props.Title}</h3>
             <img src={props.image} alt="" className="data_img"/>
             </div>
             <label className="data_loc"><b>Location: </b> {props.Location}</label>
             <p className="para">{props.describe}</p>
             </div>
+          
        
         </div>
    
