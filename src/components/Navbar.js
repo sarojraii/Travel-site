f38ed8fe1d945ad7bc97753f6895f58c3f8c32e4
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-12 10:09:31 +0545

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const handleClick = () => setClick(!click)
             <ul className = "items" >
                 <li ><i className ="fa-solid fa-house"><Link className="nav-btn" to="Hero" spy={true} smooth={true} offset={-80} duration={500}>HOME </Link></i></li>
                 <li ><i className ="fa-solid fa-briefcase"><Link className="nav-btn" to="Service" spy={true} smooth={true} offset={-90} duration={900}>SERVICE</Link></i></li>
-                <li ><i className ="fa-solid fa-address-book"><Link className="nav-btn" to="Contact" spy={true} smooth={true} offset={50} duration={500}>CONTACT</Link></i></li>
+                <li ><i className ="fa-solid fa-address-book"><Link className="nav-btn" to="Contact" spy={true} smooth={true} offset={-50} duration={1000}>CONTACT</Link></i></li>
             </ul>
             <div className ="menu-icon" onClick={handleClick}>
                 {click ? <i className ="fa-solid fa-x"></i> : <i className ="fa-solid fa-bars"></i> }
