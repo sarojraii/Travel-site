15a49e2f619c3cca79fe46054368fb97f5ed6e15
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-12 08:14:06 +0545

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Link } from "react-scroll";
 
 export default function Navbar(){
 const [click, setClick] = useState(false)
@@ -8,12 +8,12 @@ const handleClick = () => setClick(!click)
 
     return(
         <nav className="navbar">
-            <h1><i>GHUMNE<span> HO!</span></i></h1>
+            <h1><i>IK<span>KU</span></i></h1>
            
             <ul className = "items" >
-                <li ><i className ="fa-solid fa-house"><a href ="/">HOME </a></i></li>
-                <li ><i className ="fa-solid fa-briefcase"><a href="/Service">SERVICE</a></i></li>
-                <li ><i className ="fa-solid fa-address-book"><a href="/Contact">CONTACT</a></i></li>
+                <li ><i className ="fa-solid fa-house"><Link className="nav-btn" to="Hero" spy={true} smooth={true} offset={-80} duration={500}>HOME </Link></i></li>
+                <li ><i className ="fa-solid fa-briefcase"><Link className="nav-btn" to="Service" spy={true} smooth={true} offset={-90} duration={900}>SERVICE</Link></i></li>
+                <li ><i className ="fa-solid fa-address-book"><Link className="nav-btn" to="Contact" spy={true} smooth={true} offset={50} duration={500}>CONTACT</Link></i></li>
             </ul>
             <div className ="menu-icon" onClick={handleClick}>
                 {click ? <i className ="fa-solid fa-x"></i> : <i className ="fa-solid fa-bars"></i> }
